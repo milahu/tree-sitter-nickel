033a1cfdc250476fefb29796a945547ad90ce90b
Milan Hauth <milahu@gmail.com>
2022-03-15 12:28:26 +0100

    update queries-todo/highlights.scm

--- queries-todo/highlights.scm
+++ queries-todo/highlights.scm
@@ -5,41 +5,45 @@
   "then"
   "else"
   "let"
-  "inherit"
+  ;"inherit"
   "in"
-  "rec"
-  "with" 
-  "assert" 
+  ;"rec"
+  ;"with" 
+  ;"assert" 
 ] @keyword
 
 ((identifier) @variable.builtin
- (#match? @variable.builtin "^(__currentSystem|__currentTime|__nixPath|__nixVersion|__storeDir|builtins|false|null|true)$")
- (#is-not? local))
+  ;(#match? @variable.builtin "^(__currentSystem|__currentTime|__nixPath|__nixVersion|__storeDir|builtins|false|null|true)$")
+  (#match? @variable.builtin "^(false|true)$")
+(#is-not? local))
 
 ((identifier) @function.builtin
- (#match? @function.builtin "^(__add|__addErrorContext|__all|__any|__appendContext|__attrNames|__attrValues|__bitAnd|__bitOr|__bitXor|__catAttrs|__compareVersions|__concatLists|__concatMap|__concatStringsSep|__deepSeq|__div|__elem|__elemAt|__fetchurl|__filter|__filterSource|__findFile|__foldl'|__fromJSON|__functionArgs|__genList|__genericClosure|__getAttr|__getContext|__getEnv|__hasAttr|__hasContext|__hashFile|__hashString|__head|__intersectAttrs|__isAttrs|__isBool|__isFloat|__isFunction|__isInt|__isList|__isPath|__isString|__langVersion|__length|__lessThan|__listToAttrs|__mapAttrs|__match|__mul|__parseDrvName|__partition|__path|__pathExists|__readDir|__readFile|__replaceStrings|__seq|__sort|__split|__splitVersion|__storePath|__stringLength|__sub|__substring|__tail|__toFile|__toJSON|__toPath|__toXML|__trace|__tryEval|__typeOf|__unsafeDiscardOutputDependency|__unsafeDiscardStringContext|__unsafeGetAttrPos|__valueSize|abort|baseNameOf|derivation|derivationStrict|dirOf|fetchGit|fetchMercurial|fetchTarball|fromTOML|import|isNull|map|placeholder|removeAttrs|scopedImport|throw|toString)$")
- (#is-not? local))
+  ; TODO command to generate this string
+  ;(#match? @function.builtin "^(__add|__addErrorContext|__all|__any|__appendContext|__attrNames|__attrValues|__bitAnd|__bitOr|__bitXor|__catAttrs|__compareVersions|__concatLists|__concatMap|__concatStringsSep|__deepSeq|__div|__elem|__elemAt|__fetchurl|__filter|__filterSource|__findFile|__foldl'|__fromJSON|__functionArgs|__genList|__genericClosure|__getAttr|__getContext|__getEnv|__hasAttr|__hasContext|__hashFile|__hashString|__head|__intersectAttrs|__isAttrs|__isBool|__isFloat|__isFunction|__isInt|__isList|__isPath|__isString|__langVersion|__length|__lessThan|__listToAttrs|__mapAttrs|__match|__mul|__parseDrvName|__partition|__path|__pathExists|__readDir|__readFile|__replaceStrings|__seq|__sort|__split|__splitVersion|__storePath|__stringLength|__sub|__substring|__tail|__toFile|__toJSON|__toPath|__toXML|__trace|__tryEval|__typeOf|__unsafeDiscardOutputDependency|__unsafeDiscardStringContext|__unsafeGetAttrPos|__valueSize|abort|baseNameOf|derivation|derivationStrict|dirOf|fetchGit|fetchMercurial|fetchTarball|fromTOML|import|isNull|map|placeholder|removeAttrs|scopedImport|throw|toString)$")
+  (#match? @function.builtin "^(import)$")
+  (#is-not? local))
 
 [
-  (string)
-  (indented_string)
+  (StrChunks)
+  (StrBlockChunks)
 ] @string
 
-[
-  (path)
-  (hpath)
-  (spath)
-] @string.special.path
+;[
+;  (path)
+;  (hpath)
+;  (spath)
+;] @string.special.path
 
-(uri) @string.special.uri
+;(uri) @string.special.uri
 
 [
-  (integer)
-  (float)
+  ;(integer)
+  ;(float)
+  (Num)
 ] @number
 
 (interpolation
-  "${" @punctuation.special
+  "%{" @punctuation.special
   "}" @punctuation.special) @embedded
 
 (escape_sequence) @escape
@@ -48,9 +52,9 @@
   universal: (identifier) @variable.parameter
 )
 
-(formal
-  name: (identifier) @variable.parameter
-  "?"? @punctuation.delimiter)
+;(formal
+;  name: (identifier) @variable.parameter
+;  "?"? @punctuation.delimiter)
 
 (app
   function: [
@@ -70,7 +74,7 @@
 (inherit attrs: (attrs_inherited (identifier) @property) )
 
 [
-  ";"
+  ;";" ; not used in nickel?
   "."
   ","
 ] @punctuation.delimiter

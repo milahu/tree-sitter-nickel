0723020e2917aedf3f81ef39360f78205422ea59
Milan Hauth <milahu@gmail.com>
2022-03-15 12:33:06 +0100

    fix queries/highlights.scm

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -12,12 +12,12 @@
   ;"assert" 
 ] @keyword
 
-((identifier) @variable.builtin
+((Id) @variable.builtin
   ;(#match? @variable.builtin "^(__currentSystem|__currentTime|__nixPath|__nixVersion|__storeDir|builtins|false|null|true)$")
   (#match? @variable.builtin "^(false|true)$")
 (#is-not? local))
 
-((identifier) @function.builtin
+((Id) @function.builtin
   ; TODO command to generate this string
   ;(#match? @function.builtin "^(__add|__addErrorContext|__all|__any|__appendContext|__attrNames|__attrValues|__bitAnd|__bitOr|__bitXor|__catAttrs|__compareVersions|__concatLists|__concatMap|__concatStringsSep|__deepSeq|__div|__elem|__elemAt|__fetchurl|__filter|__filterSource|__findFile|__foldl'|__fromJSON|__functionArgs|__genList|__genericClosure|__getAttr|__getContext|__getEnv|__hasAttr|__hasContext|__hashFile|__hashString|__head|__intersectAttrs|__isAttrs|__isBool|__isFloat|__isFunction|__isInt|__isList|__isPath|__isString|__langVersion|__length|__lessThan|__listToAttrs|__mapAttrs|__match|__mul|__parseDrvName|__partition|__path|__pathExists|__readDir|__readFile|__replaceStrings|__seq|__sort|__split|__splitVersion|__storePath|__stringLength|__sub|__substring|__tail|__toFile|__toJSON|__toPath|__toXML|__trace|__tryEval|__typeOf|__unsafeDiscardOutputDependency|__unsafeDiscardStringContext|__unsafeGetAttrPos|__valueSize|abort|baseNameOf|derivation|derivationStrict|dirOf|fetchGit|fetchMercurial|fetchTarball|fromTOML|import|isNull|map|placeholder|removeAttrs|scopedImport|throw|toString)$")
   (#match? @function.builtin "^(import)$")
@@ -42,36 +42,38 @@
   (Num)
 ] @number
 
-(interpolation
+(RichTerm
   "%{" @punctuation.special
   "}" @punctuation.special) @embedded
 
-(escape_sequence) @escape
+; TODO
+; (escape_sequence) @escape
 
 (function
-  universal: (identifier) @variable.parameter
+  universal: (Id) @variable.parameter
 )
 
 ;(formal
-;  name: (identifier) @variable.parameter
+;  name: (Id) @variable.parameter
 ;  "?"? @punctuation.delimiter)
 
-(app
+(App
   function: [
-    (identifier) @function
+    (Id) @function
     (select
       attrpath: (attrpath
         attr: (attr_identifier) @function .))])
 
 
-(unary
+(UnaryOp
   operator: _ @operator)
 
-(binary
+(BinaryOp
   operator: _ @operator)
 
 (attr_identifier) @property
-(inherit attrs: (attrs_inherited (identifier) @property) )
+
+;(inherit attrs: (attrs_inherited (Id) @property) )
 
 [
   ;";" ; not used in nickel?
@@ -88,4 +90,4 @@
   "}"
 ] @punctuation.bracket
 
-(identifier) @variable
+(Id) @variable
